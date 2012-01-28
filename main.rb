c5f5c2cc52edbc39217d75841f50a32e4260caf6
Andrew Pepler <info@vertism.com>
2012-01-28 01:46:20 +0000

    auto detects strip width

--- main.rb
+++ main.rb
@@ -5,9 +5,7 @@ require './unshredder'
 if __FILE__ == $0
   if ARGV.count == 0
     puts 'You must provide an image to be unshredded'
-  elsif ARGV.count == 1
-    puts 'You must provide a strip width'
   else
-    uns = Unshredder.new(ARGV[0], ARGV[1])
+    uns = Unshredder.new(ARGV[0])
   end
 end  
--- unshredder.rb
+++ unshredder.rb
@@ -1,60 +1,103 @@
 #!/usr/bin/env ruby
 
-require 'net/http'
-require 'uri'
 require 'RMagick'
 
 class Unshredder < Object
-
-  def initialize(file, strip_width)
-    @file = file
-    @strips = []
-    @sorted_strips = []
-    @compare_no = 1
-
-    
-    if !FileTest.exists?(@file)
+  
+  def initialize(filename)
+    if !FileTest.exists?(filename)
       puts 'File does not exist'
       return
     end
     
-    get_strips()
-    sort_strips()
-    puts "output"
-    output_image()
+    @filename = filename
+    @shredded_image = Magick::Image::read(filename).first
+    @strip_width = get_strip_width(@image)
+    
+    if @strip_width == 0
+      puts 'Could not determine strip width'
+      return
+    else
+      puts "strips are #{@strip_width} pixels wide"
+    end
+    
+    @strips = get_strips
+    @sorted_strips = sort_strips
+    output_image
+    
+    puts 'type "open ' + @unshredded_filename + '" to view reconstructed image'
+  end
+  
+  # calculates width of strips in image
+  # 1. gets array of differences in pixels between each pair of 1 pixel wide slithers
+  # 2. gets average of these differences
+  # 3. looks for value of every nth difference in array starting with n=3
+  # 4. if > 66% are above average then returns n
+  # This calculation could be more robust but it works for all my test images
+  def get_strip_width(img)
+    differences = []
+    
+    current_slither = @shredded_image.crop(0, 0, 1, @shredded_image.rows, true)
+    x_pos = 1
+    
+    while x_pos < @shredded_image.columns
+      next_slither = @shredded_image.crop(x_pos, 0, 1, @shredded_image.rows, true)
+      differences.push(image_difference(current_slither,next_slither))
+      
+      x_pos += 1
+      current_slither = next_slither
+    end
+    
+    average_difference = differences.inject(0, :+)/@shredded_image.columns
+    
+    (3..@shredded_image.columns/2).to_a.each do |distance|
+      above_average_total = 0
+      differences.each_with_index do |slither_difference, index|
+        if (index + 1) % distance == 0 && slither_difference > average_difference
+          above_average_total += 1
+        end
+      end
+      if above_average_total * 1.5 > @shredded_image.columns / distance
+        return distance 
+      end
+    end
+    
+    0
+  end
+  
+  def image_difference(a, b)
+    a.difference(b)[1]
   end
 
-  def get_strips()
-    img = Magick::Image::read(@file).first
-    
+  def get_strips
     x_pos = 0
     id = 0
-    
-    @image_height = img.rows
-    @image_width = img.columns
-    
-    while x_pos < @image_width
-      cropped_image = img.crop(x_pos, 0, @strip_width, @image_height, true)
-      left = cropped_image.crop(0, 0, 1, @image_height, true)
-      right = cropped_image.crop(@strip_width - 1, 0, 1, @image_height, true)
-      @strips.push({:id => id, :image => cropped_image, :left => left, :right => right})
+    strips = []
+  
+    while x_pos < @shredded_image.columns
+      cropped_image = @shredded_image.crop(x_pos, 0, @strip_width, @shredded_image.rows, true)
+      left = cropped_image.crop(0, 0, 1, @shredded_image.rows, true)
+      right = cropped_image.crop(@strip_width - 1, 0, 1, @shredded_image.rows, true)
+      strips.push({:id => id, :image => cropped_image, :left => left, :right => right})
       x_pos += @strip_width
       id += 1
     end
+    
+    strips
   end  
   
-  def sort_strips()   
-    strip = find_first()
-    @sorted_strips = sort([strip], :right)
+  def sort_strips
+    strip = find_first
+    sort([strip], :right)
   end
   
-  def find_first()
+  def find_first
     worst_strip = nil
     worst_difference = 0
     
     @strips.each do |strip|
       @strips.each do |compare|
-        difference = strip[:left].difference(compare[:right])[@compare_no]
+        difference = image_difference(strip[:left],compare[:right])
         if difference > worst_difference
           worst_difference = difference
           worst_strip = strip
@@ -101,9 +144,9 @@ class Unshredder < Object
     @strips.each do |compare|
       if compare[:id] != strip[:id]
         if direction == :left
-          difference = strip[:left].difference(compare[:right])[@compare_no]
+          difference = image_difference(strip[:left],compare[:right])
         else
-          difference = strip[:right].difference(compare[:left])[@compare_no]
+          difference = image_difference(strip[:right],compare[:left])
         end
         
         if difference < min_diff
@@ -117,13 +160,11 @@ class Unshredder < Object
 
   def output_image
     il = Magick::ImageList.new
-    puts @sorted_strips.size
     @sorted_strips.each do |strip|
-      puts strip[:id]
       il.push(strip[:image])
     end
-    ext = File.extname @file
-    output_file = "#{File.dirname(@file)}/#{File.basename(@file, ext)}_unshredded#{ext}"
-    il.append(false).write(output_file)
+    ext = File.extname @filename
+    @unshredded_filename = "#{File.dirname(@filename)}/#{File.basename(@filename, ext)}_unshredded#{ext}"
+    il.append(false).write(@unshredded_filename)
   end
 end
